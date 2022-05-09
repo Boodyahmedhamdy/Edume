43cba9b456df150e378a389acffb2b78638c20f6
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-09 21:24:06 +0200

    applying singleton design pattern

--- Code/application/app/classes/Admin.class.php
+++ Code/application/app/classes/Admin.class.php
@@ -3,6 +3,15 @@
 
 class Admin extends User {
     
+    private static $admin;
+
+    public static function get_instance() 
+    {
+        if(!isset(self::$admin)) {
+            self::$admin = new Admin();
+        }
+        return self::$admin;
+    }
 
 }
 
