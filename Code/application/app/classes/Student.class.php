26f85c6f7ec2b08b32e4958b7b410aa710eb0f6d
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-10 22:23:24 +0200

    the main customer for use

--- Code/application/app/classes/Student.class.php
+++ Code/application/app/classes/Student.class.php
@@ -3,9 +3,55 @@
 
 class Student extends User implements IStudent {
 
+    // attributes
+    private float $gpa;
 
 
+    // getters
+    // -----------
+    public function get_gpa()
+    {
+        return $this->gpa;
+    }
+
+
+
+    // setters
+    // -------------
+    public function set_gpa(float $gpa)
+    {
+        $this->gpa = $gpa;
+    }
+
+
+
+
+    // constructor
+    // --------------------
+    public function __construct(int $id, string $email,
+                                string $username, 
+                                string $password, 
+                                string $name)
+    {
+        parent::__construct($id, $email, $username, $password, $name, 
+        Role::STUDENT);
+    }
+
+
+
+    // functionallity
+    // ------------------
+    public function enroll_in_course(Course $course)
+    {
+        # code...
+    }
+
+    public function drop_course(Course $course)
+    {
+        # code...
+    }
     
+
 
 
 
