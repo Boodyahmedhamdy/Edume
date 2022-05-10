5a686d9818cc02e3f7b8399d24e6e338fa3c3301
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-10 22:20:12 +0200

    applyed the singleton design pattern

--- Code/application/app/classes/Admin.class.php
+++ Code/application/app/classes/Admin.class.php
@@ -3,16 +3,29 @@
 
 class Admin extends User {
     
+    // singleton design pattern
     private static $admin;
 
-    public static function get_instance() 
+    private function __construct(int $id, string $email,
+                                string $username, 
+                                string $password, 
+                                string $name)
+    {
+        parent::__construct($id, $email, $username, $password, $name, 
+        Role::ADMIN);
+
+    }
+
+    public static function get_instance(int $id, string $email,
+                                string $username, 
+                                string $password, 
+                                string $name) 
     {
         if(!isset(self::$admin)) {
-            self::$admin = new Admin();
+            self::$admin = new Admin($id, $email, $username, $password, $name);
         }
         return self::$admin;
     }
 
 }
 
-$ahmed = new Admin();
