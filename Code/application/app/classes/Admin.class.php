18de3010b4fca26eb2024ccc94aa89e8b2be77c8
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-11 22:29:25 +0200

    getters, setters, constructor and functionallity -> (add, delete, update) users

--- Code/application/app/classes/Admin.class.php
+++ Code/application/app/classes/Admin.class.php
@@ -27,5 +27,76 @@ class Admin extends User {
         return self::$admin;
     }
 
-}
 
+    // functionallity
+    // -----------------------------
+
+    // adding Users
+    public function add_student(Student $student)
+    {
+        array_push($this->students, $student);
+    }
+
+    public function add_teacher(Teacher $teacher)
+    {
+        array_push($this->teachers, $teacher);
+    }
+
+    public function add_organizatin(Organization $organization)
+    {
+        array_push($this->organizations, $organization);
+
+    }
+
+    // deleting Users
+    public function delete_student(Student $student)
+    {
+        $Student = array_filter($Student, function ($element) use ($student)
+        { 
+            return ($element != $student);
+        }
+            ); 
+
+    }
+
+    public function delete_teacher(Teacher $teacher)
+    {
+        $Teacher = array_filter($Teacher, function ($element) use ($teacher)
+        { 
+            return ($element != $teacher);
+        }
+            );
+    }
+
+    public function delete_organization(Organization $organization)
+    {
+        $Organization = array_filter($Organization, function ($element) use ($organization)
+        { 
+            return ($element != $organization);
+        }
+            );
+    }
+
+    // updating Users
+    public function update_student(Student $student)
+    {
+        # code...
+    }
+
+    public function update_teacher(Teacher $teacher)
+    {
+        # code...
+    }
+
+    public function update_organization(Organization $organization)
+    {
+        # code...
+    }
+
+    // genrating reports
+    public function generate_report()
+    {
+        # code...
+    }
+
+}
