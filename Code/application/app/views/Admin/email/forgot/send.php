60c99ea7fb31b0daa8eb6eb7e0575acd06c9c3c4
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-16 20:05:20 +0200

    the code for sending emails

---
+++ Code/application/app/views/Admin/email/forgot/send.php
@@ -0,0 +1,296 @@
+<?php 
+
+	include "mail.php";
+	//send_mail($recipient,$subject,$message);
+
+	$error = "";
+
+	if(count($_POST) > 0)
+	{
+
+		//something was posted
+		$recipient = $_POST['email'];
+		$subject = $_POST['subject'];
+		$message = $_POST['message'];
+
+		if(empty($recipient)){
+			$error .= "recipient can not be empty<br>";
+		}
+
+		if(empty($subject)){
+			$error .= "subject can not be empty<br>";
+		}
+
+		if(empty($message)){
+			$error .= "message can not be empty<br>";
+		}
+		
+		if($error == "")
+		{
+			if(send_mail($recipient,$subject,$message))
+			{
+				$error .= "Message sent!<br>";
+			}else
+			{
+				$error .= "Message NOT sent!<br>";
+			}
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Send An Email</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../../../assets/img/favicon.png" rel="icon">
+  <link href="../../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../../../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../../../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../../../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../../../assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="../../index.php" class="logo d-flex align-items-center">
+        <img src="../../../assets/img/logo.png" alt="">
+        <span class="d-none d-lg-block">Edume</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+    <div class="search-bar">
+      <form class="search-form d-flex align-items-center" method="POST" action="#">
+        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+        <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+      </form>
+    </div><!-- End Search Bar -->
+
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item d-block d-lg-none">
+          <a class="nav-link nav-icon search-bar-toggle " href="#">
+            <i class="bi bi-search"></i>
+          </a>
+        </li><!-- End Search Icon-->
+
+ 
+
+        <li class="nav-item dropdown pe-3">
+
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="../../../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Kevin Anderson</h6>
+              <span>Web Designer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="../../index.php">
+              <i class="ri-home-heart-line"></i>
+              <span>Home</span>
+            </a>
+          </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="ri-add-fill"></i><span>Add</span><i class="bi bi-chevron-down ms-auto"></i>
+         
+        </a>
+        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="../../add_student.php">
+              <i class="bi bi-circle"></i><span>Student</span>
+            </a>
+          </li>
+          <li>
+            <a href="../../add_teacher.php">
+              <i class="bi bi-circle"></i><span>Teacher</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="send.php">
+          <i class="bi bi-chat-text-fill"></i>
+          <span>Send An Email</span>
+        </a>
+      </li>
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Send an Email</h5>
+                  </div>
+
+                  <style type="text/css">
+		
+		
+
+		form input{
+			width: 270px;
+			padding: 10px;
+			border: solid thin #aaa;
+			border-radius: 10px;
+			margin: 5px;
+			outline: none;
+		}
+
+		.btn{
+
+			width: 290px;
+			cursor: pointer;
+		}
+
+		.text{
+			border: solid thin #aaa;
+			border-radius: 10px;
+			border: solid thin #aaa;
+			width: 270px;
+			margin-left: 5px;
+			padding: 10px;
+		}
+
+	</style>
+
+	<form method="post">
+		
+		<div>
+			<?php if($error != ""):?>
+				<span style="color: red;"><?=$error?></span>
+			<?php endif;?>
+		</div>
+
+        <div class="col-12">
+            <label for="ReceiverEmail" class="form-label">Receiver Email</label>
+            <input type="text" name="email" autofocus="true" required><br> 
+        </div>
+         
+        <div class="col-12">
+            <label for="Subject" class="form-label">Subject</label>
+            <input type="text" name="subject" required><br> 
+        </div>
+		
+        <div class="col-12">
+            <label for="Message" class="form-label">Message</label>
+            <textarea class="text" name="message" required></textarea><br><br>
+        </div>
+		
+		
+		<div class="col-12">
+            <button class="btn btn-primary w-100" type="submit">Send</button>
+        </div>
+
+	</form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-php-template/ -->
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="../../../assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="../../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../../assets/vendor/chart.js/chart.min.js"></script>
+<script src="../../../assets/vendor/echarts/echarts.min.js"></script>
+<script src="../../../assets/vendor/quill/quill.min.js"></script>
+<script src="../../../assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="../../../assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="../../../assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="../../../assets/js/main.js"></script>
+
+</body>
+
+</html>
