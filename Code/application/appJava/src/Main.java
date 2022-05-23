21083ff1261e2d7b72d3cad911b986c4bcca5ecf
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-23 20:36:10 +0200

    trying to connect to database -- still not working

--- Code/application/appJava/src/Main.java
+++ Code/application/appJava/src/Main.java
@@ -1,11 +1,38 @@
 import Core.Student;
+import Database.SQLRunner;
+
+import java.sql.*;
 
 public class Main {
 
 
-    public static void main(String[] args) {
-        System.out.println("something");
+    public static void main(String[] args) throws SQLException {
+//        System.out.println("something");
+//        ResultSet rs = SQLRunner.run("select * from students");
+//        while(rs.next()){
+//            for(int colNumber = 1 ; colNumber < 8 ; colNumber++) {
+//                System.out.print(rs.getString(colNumber) + " -> ");
+//            }
+//        }
 
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:sqlserver:localhost;Database=Edume",
+                    "boody",
+                    "root");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from students");
+
+            while(resultSet.next()){
+            for(int colNumber = 1 ; colNumber < 8 ; colNumber++) {
+                System.out.print(resultSet.getString(colNumber) + " -> ");
+            }
+        }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
     }
 
 
